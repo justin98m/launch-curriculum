55fa1e80ad6f261a3ea515e3d51a2b60a115c997
Justin Cameron <justin1998m1@gmail.com>
2022-06-15 15:53:37 -0400

    bonus calculator

--- js101/lesson_2/calculator.js
+++ js101/lesson_2/calculator.js
@@ -1,27 +1,69 @@
 const readline = require('readline-sync');
-//Ask User for two nums
-console.log(`Welcome to Calculator`);
+const message = require('./calculator-messages.json');
+console.log(message);
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
 
-let num1 = parseInt(readline.question(`What is your first number? \n`));
-let num2 = parseInt(readline.question(`What is your second number \n`));
-console.log(num1,num2);
-//Ask user to add subtreact multiply or divide the two numbers
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
 
-let operation = readline.question(`What operation would you like to preform? \n
-  1)Addition 2)Subtration 3)Muliplication 4)Division \n`);
-let result;
- switch(operation) {
-   case '1':
-     result = num1 + num2;
-     break;
-   case '2':
-     result = num1 - num2;
-     break;
-   case '3':
-     result = num1 * num2;
-     break;
-   case '4':
-     result = num1 / num2;
-     break;
-   }
-console.log(`The result is  ${result}`);
+prompt(message.welcome);
+function newCalculation() {
+  prompt(message.getVal1);
+  let number1 = readline.question();
+
+  while (invalidNumber(number1)) {
+    prompt(message.invalidNum);
+    number1 = readline.question();
+  }
+
+  prompt(message.getVal2);
+  let number2 = readline.question();
+
+  while (invalidNumber(number2)) {
+    prompt(message.invalidNum);
+    number2 = readline.question();
+  }
+
+  prompt(message.getOperation);
+  prompt(message.operationKey);
+  let operation = readline.question();
+
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt(message.invalidOperation);
+    operation = readline.question();
+  }
+
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  prompt(`${message.result} ${output}`);
+}
+newCalculation();
+let keepGoing = true;
+let response = "";
+while (keepGoing) {
+  prompt(message.restart);
+  response = readline.question();
+  response = response.toLowerCase();
+  if (response === 'y') {
+    newCalculation();
+  } else if (response === 'n') {
+    keepGoing = false;
+  }
+}
