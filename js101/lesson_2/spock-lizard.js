683410f276e99e6af9be8827bd87f6af1bd5d284
Justin Cameron <justin1998m1@gmail.com>
2022-07-01 16:19:31 -0400

    Fixed best of 5 functionality

--- js101/lesson_2/spock-lizard.js
+++ js101/lesson_2/spock-lizard.js
@@ -1,11 +1,52 @@
 const readline = require("readline-sync");
-const VALID_CHOICES = ['rock','paper','scissors','spock','lizard'];
+const MAX_ABBREVIATION_LENGTH = 2;
+const VALID_WORD_CHOICES = ['rock','paper','scissors','spock','lizard'];
+const VALID_ABBREVIATION_CHOICES = ['sp','r','sc','l','p'];
+const MAX_WINS = 3;
+let playerWins = 0;
+let computerWins = 0;
+
 
 function prompt (message) {
   console.log(`=> ${message}`);
 }
+function isAbbreviation (choice) {
+  return choice.length <= MAX_ABBREVIATION_LENGTH;
+}
+function convertAbbreviation (choice) {
+  switch (choice) {
+    case 'sp':
+      return 'spock';
+    case 'r':
+      return 'rock';
+    case 'sc':
+      return 'scissors';
+    case 'l':
+      return 'lizard';
+    case 'p':
+      return 'paper';
+  }
+}
+function addWin (handWinner) {
+  if (handWinner === true) {
+    playerWins++;
+  } else if (handWinner === false) {
+    computerWins++;
+  }
+}
+function decideMatchWinner () {
+  if (playerWins ===  MAX_WINS) {
+    displayMatchWinner('player');
+    return true;
+  } else if (computerWins === MAX_WINS) {
+    displayMatchWinner('computer');
+    return true;
+  }
+  //no winner can be decded yet
+  return false;
+}
 //returns true for a user win, false for a cpu win and "tie"
-function decideWinner (choice,computerChoice) {
+function decideWinningHand (choice,computerChoice) {
   if (choice === computerChoice) {
     return "tie";
   }
@@ -43,38 +84,71 @@ function playerHasLizard (computerChoice) {
   if (computerChoice === "paper" || computerChoice === "spock") return true;
   return false;
 }
-
-function displayWinner (winner) {
+function verifyInput (choice) {
+  while (!VALID_WORD_CHOICES.includes(choice) && !VALID_ABBREVIATION_CHOICES.
+    includes(choice)) {
+    prompt(`${choice} is not a valid response`);
+    prompt(`Choose rock(r), paper(p), scissors(sc), spock(sp) or lizard(l)`);
+    choice = readline.question().toLowerCase();
+  }
+  return choice;
+}
+function displayMatchWinner (winner) {
+  if (winner === 'player') {
+    prompt("You win the Match!");
+  } else {
+    prompt("The Computer wins this match");
+  }
+}
+function displayWinningHand (winner) {
   if (winner === true) {
-    prompt("You win!");
+    prompt("You win this hand!");
   } else if (winner === false) {
-    prompt("The Computer Wins");
+    prompt("The Computer wins this hand");
   } else {
     prompt("It's a tie");
   }
 }
+function playAgain() {
+  prompt('Would you like to play again(y/n)\n');
+  let playAnother = readline.question().toLowerCase();
+  while (playAnother !== 'n' && playAnother !== 'y') {
+    prompt('Please enter y or n\n');
+    playAnother = readline.question().toLowerCase();
+  }
+  if (playAnother === 'n') {
+    return false;
+  }
+  return true;
+}
 while (true) {
-  prompt(`Choose ${VALID_CHOICES.join(', ')}`);
-  let choice = readline.question();
-
-  while (!VALID_CHOICES.includes(choice)) {
-    prompt(`${choice} is not a valid response`);
-    prompt(`Choose rock, paper, scissors, spock or lizard`);
-    choice = readline.question();
+  prompt("Let's Play rock, paper, scissors, spock , lizard. Best of 5");
+  prompt(`Choose rock(r) , paper(p), scissors(sc) , spock(sp) or lizard(l)\n`);
+  let choice = readline.question().toLowerCase();
+  choice = verifyInput(choice);
+  if (isAbbreviation(choice)) {
+    choice = convertAbbreviation(choice);
   }
 
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
+  let randomIndex = Math.floor(Math.random() * VALID_WORD_CHOICES.length);
+  let computerChoice = VALID_WORD_CHOICES[randomIndex];
 
   prompt(`Your Choice : ${choice}`);
   prompt(`Computer's Choice : ${computerChoice}\n`);
-  let winner = decideWinner(choice,computerChoice);
-  displayWinner(winner);
-  prompt('Would you like to play again(y/n)');
-  let playAgain = readline.question().toLowerCase();
-  while (playAgain !== 'n' && playAgain !== 'y') {
-    prompt('Please enter y or n');
-    playAgain = readline.question().toLowerCase();
+  let handWinner = decideWinningHand(choice,computerChoice);
+  displayWinningHand(handWinner);
+  addWin(handWinner);
+  prompt(`Your Wins ${playerWins} \n CPU Wins ${computerWins}\n`);
+
+  if (decideMatchWinner()) {
+    let playAnother = playAgain();
+    if (!playAnother) {
+      break;
+    } else {
+      playerWins = 0;
+      computerWins = 0;
+    }
+
   }
-  if (playAgain === 'n') break;
+
 }
