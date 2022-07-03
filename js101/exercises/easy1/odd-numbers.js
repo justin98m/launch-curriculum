188d81727fc6b5c4f0361d258df99394afb564b2
Justin Cameron <justin1998m1@gmail.com>
2022-07-03 11:37:27 -0400

    Allowed for user to indicate range of values

--- js101/exercises/easy1/odd-numbers.js
+++ js101/exercises/easy1/odd-numbers.js
@@ -1,6 +1,31 @@
 //Log all odd nums from 1 to 99 inclusive
 //seperate each num on its own line
-let max = 99
-for (i=1;i<=99;i+=2 ){
+//provide way for use to specify limits of range of nums
+const readline = require("readline-sync");
+const MAXIMUM_VALUE = 1000;
+const MINIMUM_VALUE = -1000;
+let min,max;
+console.log(`I will print out all the odd numbers from the range you give me`);
+
+while (true) {
+  console.log(`Enter your min value: `);
+  min = Number(readline.question());
+  console.log(`Enter your max value: `);
+  max = Number(readline.question());
+  if (min >= max){
+    console.log(`Your Min value must be less than your max value` );
+  } else if (min < MINIMUM_VALUE){
+    console.log(`Your min value cannot be less than ${MINIMUM_VALUE}`);
+  } else if (max > MAXIMUM_VALUE){
+    console.log(`Your max value cannot be more than ${MAXIMUM_VALUE}`);
+  } else{
+    break;
+  }
+}
+
+//min should truly equal the smallest odd val within the range
+if(min % 2 === 0)min++;
+
+for (i = min; i <= max;i += 2){
   console.log(`\n${i}`);
 }
