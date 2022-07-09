f20f521cd179986462d3ca37ed961f387972558f
Justin Cameron <justin1998m1@gmail.com>
2022-07-08 22:21:30 -0400

    q4 and q5 completion

---
+++ js101/lesson_3/easy_2/q4.js
@@ -0,0 +1,6 @@
+//starting with the following string at 'Four score and ' in front of it (2 ways)
+
+let famousWords = "seven years ago ...";
+
+console.log("Four score and " + famousWords );
+console.log("Four score and ".concat(famousWords));
